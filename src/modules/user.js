7543b15211030dfe39c776b8810cf9aa207e9634
Theo Cho <cdsleaf@gmail.com>
2022-01-16 16:18:50 +1300

    implement logout

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,13 +1,16 @@
 import { createAction, handleActions } from 'redux-actions';
 import { takeLatest } from 'redux-saga/effects';
+import { callExpression } from '../../../../../Library/Caches/typescript/4.5/node_modules/@babel/types/lib/index';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga';
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER';
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes('user/CHECK');
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -19,9 +22,19 @@ function checkFailureSaga() {
   }
 }
 
+function* logoutSaga() {
+  try {
+    yield callExpression(authAPI.logout);
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -44,6 +57,10 @@ export default handleActions(
       ...state,
       user,
       checkError: null,
+    }),
+    [LOGOUT]: state => ({
+      ...state,
+      user: null,
     }),
   },
   initialState,
